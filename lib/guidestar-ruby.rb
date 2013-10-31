da0999c6e1985c7e5a3d7198ed231d30c1924295
kingjason <jason.king@covenanteyes.com>
2013-10-30 21:52:31 -0400

    Update guidestar-ruby.rb

--- lib/guidestar-ruby.rb
+++ lib/guidestar-ruby.rb
@@ -7,7 +7,7 @@ require 'guidestar/client'
 
 module Guidestar
   VERSION = File.read(File.join(File.dirname(__FILE__), '..', 'VERSION'))
-  ENDPOINT = "https://gsservices.guidestar.org/GuideStar_SearchService/SearchService.asmx/GuideStarDetail"
+  ENDPOINT = "https://gsservices.guidestar.org/GuideStar_SearchService/SearchService.asmx/GuideStarBasic"
 
   class GuidestarError < StandardError; end
   class InsufficientArgs < GuidestarError; end
